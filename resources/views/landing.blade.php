b7defa456bdf1248589603caa7e9c46a987b792e
TitamSeptian <dwiputra4456@protonmail.com>
2021-12-13 16:46:31 +0700

    update landing page

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -20,6 +20,7 @@
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/font-awesome.min.css">
     <link rel="stylesheet" href="my-style.css">
+    <link rel="icon" type="image/png" href="{{ asset('logo-sirekan.png') }}" />
 </head>
 
 <body class="">
@@ -35,7 +36,7 @@
         <div id="navbarSupportedContent" class="collapse navbar-collapse">
             <ul class="navbar-nav ml-auto">
                 <li class="nav-item"><a href="#" class="nav-link text-uppercase font-weight-bold">Home</a></li>
-                <li class="nav-item "><a href="#" class="nav-link text-uppercase font-weight-bold">Prediksi</a>
+                <li class="nav-item "><a href="#" class="nav-link text-uppercase font-weight-bold">Rekomendasi</a>
                 </li>
                 <li class="nav-item "><a href="#" class="nav-link text-uppercase font-weight-bold">About</a>
                 </li>
@@ -53,6 +54,131 @@
             <div class="btn btn-color-1 w-25">Info Selengkapnya</div>
         </div>
     </div>
+    <div class="container">
+        <div class="form-makanan py-2 text-center">
+            <h3 class="font-weight-bold">Bagaimana Mood kamu ?</h3>
+            <div class="d-flex justify-content-center align-items-center">
+
+                <label>
+                    <input type="radio" class="custom-radio" name="mood" />
+                    <div class="box">
+                        <span>Sedih</span>
+                    </div>
+                </label>
+
+                <label class="mx-2">
+                    <input type="radio" class="custom-radio" name="mood" />
+                    <div class="box">
+                        <span>Biasa aja</span>
+                    </div>
+                </label>
+                <label>
+                    <input type="radio" class="custom-radio" name="mood" />
+                    <div class="box">
+                        <span>Senang</span>
+                    </div>
+                </label>
+            </div>
+        </div>
+        <div class="form-makanan py-2 text-center">
+            <h3 class="font-weight-bold">Berkuah atau tidak</h3>
+            <div class="d-flex justify-content-center align-items-center">
+
+                <label>
+                    <input type="radio" class="custom-radio" name="kuah" />
+                    <div class="box">
+                        <span>Tidak</span>
+                    </div>
+                </label>
+
+                <label class="mx-2">
+                    <input type="radio" class="custom-radio" name="kuah" />
+                    <div class="box">
+                        <span>Kuah</span>
+                    </div>
+                </label>
+            </div>
+        </div>
+        <div class="form-makanan py-2 text-center">
+            <h3 class="font-weight-bold">Pedesnya ?</h3>
+            <div class="d-flex justify-content-center align-items-center">
+
+                <label>
+                    <input type="radio" class="custom-radio" name="pedas" />
+                    <div class="box">
+                        <span>Tidak Pedas</span>
+                    </div>
+                </label>
+
+                <label class="mx-2">
+                    <input type="radio" class="custom-radio" name="pedas" />
+                    <div class="box">
+                        <span>Sedang</span>
+                    </div>
+                </label>
+                <label>
+                    <input type="radio" class="custom-radio" name="pedas" />
+                    <div class="box">
+                        <span>Pedas</span>
+                    </div>
+                </label>
+            </div>
+        </div>
+        <div class="form-makanan py-2 text-center">
+            <h3 class="font-weight-bold">Manisnya ?</h3>
+            <div class="d-flex justify-content-center align-items-center">
+
+                <label>
+                    <input type="radio" class="custom-radio" name="manis" />
+                    <div class="box">
+                        <span>Tidak Manis</span>
+                    </div>
+                </label>
+
+                <label class="mx-2">
+                    <input type="radio" class="custom-radio" name="manis" />
+                    <div class="box">
+                        <span>Sedang</span>
+                    </div>
+                </label>
+                <label>
+                    <input type="radio" class="custom-radio" name="manis" />
+                    <div class="box">
+                        <span>Manis</span>
+                    </div>
+                </label>
+            </div>
+        </div>
+        <div class="form-makanan py-2 text-center">
+            <h3 class="font-weight-bold">Budget ?</h3>
+            <div class="d-flex justify-content-center align-items-center">
+
+                <label>
+                    <input type="radio" class="custom-radio" name="harga" />
+                    <div class="box">
+                        <span>Murah</span>
+                    </div>
+                </label>
+
+                <label class="mx-2">
+                    <input type="radio" class="custom-radio" name="harga" />
+                    <div class="box">
+                        <span>Sedang</span>
+                    </div>
+                </label>
+                <label>
+                    <input type="radio" class="custom-radio" name="harga" />
+                    <div class="box">
+                        <span>Mahal</span>
+                    </div>
+                </label>
+            </div>
+        </div>
+        <button class="btn btn-color-1 btn-rounded text-center btn-block">Cari Rekomendasi</button>
+    </div>
+    <footer class="text-center mt-5 mb-2">
+        <span>Copyright Sukses Team - 3B</span>
+    </footer>
     <script src="js/jquery.min.js"></script>
     <script src="js/popper.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
